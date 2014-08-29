b54744c4fc294938c2d6ab6f0bd7b9b08a58e35b
App Academy Student <student@appacademy.io>
2014-08-29 10:45:25 -0700

    initialized framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HaveRead::Application.config.secret_key_base = '12ffb266a1bc7511761ced1aa1af9e5e11077d766d1154d7b87db9dbc320a428d90982e6760682ed180995c067d861d280d33ec38f7858494fcb24e405358f20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HaveRead::Application.config.session_store :cookie_store, key: '_have_read_session'
